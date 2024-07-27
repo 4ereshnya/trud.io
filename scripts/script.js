63734e2fd0ade8bbd40f3a2dd31793d620973424
4ereshnya <110485864+4ereshnya@users.noreply.github.com>
2024-07-27 10:56:04 +0400

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,11 @@
 document.getElementById('backButton').addEventListener('click', function() {
     window.history.back();
 });
+document.addEventListener("DOMContentLoaded", function() {
+    const link = document.querySelector('.navbar__links-text[href="#contacts"]');
+    link.addEventListener('click', function(e) {
+        e.preventDefault();  // Предотвратить стандартное действие перехода по ссылке
+        const contactsSection = document.querySelector('#contacts');
+        contactsSection.scrollIntoView({ behavior: 'smooth' });
+    });
+});
